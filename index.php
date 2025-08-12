6b6cbdd7c8c30401b93c7addeb3cad7deacb5f3f
João Medeiros <joao_m_medeiros@estudante.sesisenai.org.br>
2025-08-11 21:47:02 -0300

    add - mudança de algumas ordens

--- index.php
+++ index.php
@@ -5,11 +5,12 @@ include("conexao.php");
 $sql = "SELECT * FROM usuarios"; // Erro de SQL: FORM ao invés de FROM
 $resultado = mysqli_query($conn, $sql);
 
-echo "<h1>Lista de Usuários</h1>";
+<a href='cadastrar.php'>Cadastrar novo</a>
 
 while ($linha = mysqli_fetch_array($resultado)) {
+    echo "<h1>Lista de Usuários</h1>";
     echo "Nome: " . $linha['nome'] . "<br>";
     echo "Email: " . $linha['email'] . "<br><br>";
 }
+
 ?>
-<a href='cadastrar.php'>Cadastrar novo</a>
