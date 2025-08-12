38d017ce26d23ebd4d6aaac1c1f744014f5e026f
João Medeiros <joao_m_medeiros@estudante.sesisenai.org.br>
2025-08-11 21:04:11 -0300

    add - parenteses

--- cadastrar.php
+++ cadastrar.php
@@ -5,7 +5,7 @@ include("conexao.php");
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
     $nome = $_POST['nome'];
     $email = $_POST['email']
-
+}
     $sql = "INSERT INTO usuarios (nome, email) VALUES ('$nome', '$email')";
     $res = mysqli_query($conn, $sql);
     if ($res) {
