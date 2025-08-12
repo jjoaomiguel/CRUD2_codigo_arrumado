7f65abe0c9df7c9291f31eba9471481e51981ad0
João Medeiros <joao_m_medeiros@estudante.sesisenai.org.br>
2025-08-11 21:02:54 -0300

    add - aspas simples

--- cadastrar.php
+++ cadastrar.php
@@ -2,9 +2,9 @@
 // Cadastro com erros de sintaxe e falta de validação
 include("conexao.php");
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nome = $_POST["nome"];
-    $email = $_POST["email"]
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    $nome = $_POST['nome'];
+    $email = $_POST['email']
 
     $sql = "INSERT INTO usuarios (nome, email) VALUES ('$nome', '$email')";
     $res = mysqli_query($conn, $sql);
